1639174d63836bd0865787c9d889bb52480cc681
ffrancielicaldeirao <francieli.aparecida@homail.com>
2025-10-26 16:59:58 -0300

    Implementacao do servidor

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const host = '0.0.0.0';
+const porta = 3000;
+const servidor = express();
+
+
+servidor.listen(porta, host, () =>{
+    console.log(`Servidor escutando em http://${host}:${porta}`);
+});
